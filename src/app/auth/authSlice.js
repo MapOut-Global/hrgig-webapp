c7ddaec046694f7553bb7a84f31979e99adcee9e
MUhammed Ishan K <ishan@mapout.com>
2024-01-24 09:24:22 +0530

    added rtk query state mangement

---
+++ src/app/auth/authSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { authApiSlice } from "./authApiSlice";
+
+const initialState = {
+  token: null,
+  email: null,
+};
+
+const handleAuthFulfilled = (state, action) => {
+  const { token, email } = action.payload?.data || {};
+  state.token = token;
+  state.email = email;
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.token = null;
+      state.email = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      (action) =>
+        [
+          authApiSlice.endpoints.signUp,
+          authApiSlice.endpoints.signIn,
+          authApiSlice.endpoints.googleSignIn,
+          authApiSlice.endpoints.linkedinSignIn,
+        ].includes(action),
+      handleAuthFulfilled
+    );
+  },
+});
+
+export const { logout } = authSlice.actions;
+
+export default authSlice.reducer;
