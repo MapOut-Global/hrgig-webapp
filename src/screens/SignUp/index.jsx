c33c2897341eff9f75eb43e13fd04222a6c8c7f2
basit-mapout <basit@mapout.com>
2024-01-24 13:05:24 +0530

    added tailwind css and a few packages

--- src/screens/SignUp/index.jsx
+++ src/screens/SignUp/index.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="flex h-screen  bg-red-500">
+      <div className="w-60 h-full bg-green-500">
+       
+      </div>
+
+      {/* <div className="w-60 h-full bg-blue-500"> 
+       </div> */}
+    </div>
+  );
+};
 
 export default SignUp
