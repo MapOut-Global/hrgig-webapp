8de630b9fd635eae6355f94ca5ab8e6c23117a33
basit-mapout <basit@mapout.com>
2024-01-27 18:21:08 +0530

    updated the image as s3 url

--- src/screens/Auth/WelcomeBanner/index.jsx
+++ src/screens/Auth/WelcomeBanner/index.jsx
@@ -3,7 +3,7 @@ import WelcomeImage from '../../../assets/signupwelcomeimg.jpg'
 
 const WelcomeSection = () => {
   return (
-    <div className="w-3/4 h-screen flex items-center justify-center bg-[url('/Users/basitraza/Desktop/Mapout/hrgig-webapp/src/assets/background1.jpg')] bg-cover bg-center">
+    <div className="w-3/4 h-screen flex items-center justify-center bg-[url('https://s3.ap-south-1.amazonaws.com/s3.mapout.com/background1.jpg')] bg-cover bg-center">
     <img className='h-3/5 w-88 ' src={WelcomeImage} alt='Signup welcome' />
   </div>
   );
