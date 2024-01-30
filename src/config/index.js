b87c3fbab3d5e8c8f466644f683f502d701de26a
basit-mapout <basit@mapout.com>
2024-01-30 10:38:50 +0530

    upadted a line of code to retrigger deployment

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,3 @@
 export const LinkedinOAuthUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&scope=openid%20email%20profile&client_id=${process.env.REACT_APP_LINKEDIN_CLIENT}&redirect_uri=${process.env.REACT_APP_HOST_NAME}/linkedin`
 export const GoogleClientId = process.env.REACT_APP_HRGIG_GOOGLE_CLIENT_ID
+//
